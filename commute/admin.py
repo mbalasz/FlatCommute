240b620ddc4808753ee413160ab71531ead571e8
Mateusz Balasz <matti93@gmail.com>
2018-07-25 02:13:35 +0100

    Add address to admin site.

--- commute/admin.py
+++ commute/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from commute.models import Place, Flat, Route, User
+from commute.models import Place, Flat, Route, User, Address
 
 admin.site.register(Place)
 admin.site.register(Flat)
 admin.site.register(Route)
 admin.site.register(User)
+admin.site.register(Address)
