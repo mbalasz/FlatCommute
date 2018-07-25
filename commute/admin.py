1851a0cff42db77948d89a8582663eaccae066fa
Mateusz Balasz <matti93@gmail.com>
2018-07-25 02:10:47 +0100

    Add models to the admin page.

--- commute/admin.py
+++ commute/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from commute.models import Place, Flat, Route, User
+
+admin.site.register(Place)
+admin.site.register(Flat)
+admin.site.register(Route)
+admin.site.register(User)
