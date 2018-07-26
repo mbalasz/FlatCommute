01dc613dac29bf3f079ef610eb1542428cd8a8a3
Mateusz Balasz <matti93@gmail.com>
2018-07-27 00:51:40 +0100

    Switch to render in the view.

--- commute/views.py
+++ commute/views.py
@@ -1,13 +1,11 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from commute.models import Flat
 
 
 def index(request):
     flats = Flat.objects.all()
-    template = loader.get_template('commute/index.html')
     context = {
         'flats': flats
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'commute/index.html', context)
