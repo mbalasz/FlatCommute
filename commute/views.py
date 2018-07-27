7873f986b1d2f4753039b1f767b0ce3c90e00429
Mateusz Balasz <matti93@gmail.com>
2018-07-27 02:09:52 +0100

    Add markers for places and flats.

--- commute/views.py
+++ commute/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-from commute.models import Flat
+from commute.models import Flat, Place
 
 
 def index(request):
     flats = Flat.objects.all()
+    places = Place.objects.all()
     context = {
-        'flats': flats
+        'flats': flats,
+        'places': places,
+        'city': "London",
     }
     return render(request, 'commute/index.html', context)
